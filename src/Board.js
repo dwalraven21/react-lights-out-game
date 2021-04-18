1d268b9df50ff2b4065e27adefcdf6bdecc36df6
Danielle Walraven <dwalraven21@gmail.com>
2021-04-18 16:01:07 -0400

    completed win condition

--- src/Board.js
+++ src/Board.js
@@ -63,6 +63,7 @@ class Board extends Component {
   /** handle changing a cell: update board & determine if winner */
 
   flipCellsAround(coord) {
+    console.log("Flipping!", coord)
     let {ncols, nrows} = this.props;
     let board = this.state.board;
     let [y, x] = coord.split("-").map(Number);
@@ -75,27 +76,33 @@ class Board extends Component {
         board[y][x] = !board[y][x];
       }
     }
-
-    // TODO: flip this cell and the cells around it
+    flipCell(y,x)
+    flipCell(y, x - 1)
+    flipCell(y, x + 1)
+    flipCell(y - 1, x)
+    flipCell(y + 1, x)
 
     // win when every cell is turned off
     // TODO: determine is the game has been won
+    let hasWon = board.every(row => row.every(cell => !cell))
 
-    // this.setState({board, hasWon});
+    this.setState({board, hasWon});
   }
 
 
   /** Render game board or winning message. */
 
   render() {
-
+    if (this.state.hasWon){
+      return <h1>YOU WON!!</h1>
+    }
     // if the game is won, just show a winning msg & render nothing else
     let tblBoard = [];
     for (let y = 0; y < this.props.nrows; y++){
       let row = [];
       for (let x = 0; x < this.props.ncols; x++){
-        let coord = `${y},${x}`
-        row.push(<Cell key={coord} isLit={this.state.board[y][x]}/>)
+        let coord = `${y}-${x}`
+        row.push(<Cell key={coord} isLit={this.state.board[y][x]} flipCellsAroundMe={() => this.flipCellsAround(coord)}/>)
       }
       tblBoard.push(<tr key={y}>{row}</tr>)
     }
