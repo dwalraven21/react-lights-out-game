44a3000ebab3a54458063b0016adf6afcec641ec
Danielle Walraven <dwalraven21@gmail.com>
2021-04-18 16:40:37 -0400

    Styled

--- src/Board.js
+++ src/Board.js
@@ -2,33 +2,6 @@ import React, {Component} from "react";
 import Cell from "./Cell";
 import './Board.css';
 
-
-/** Game board of Lights out.
- *
- * Properties:
- *
- * - nrows: number of rows of board
- * - ncols: number of cols of board
- * - chanceLightStartsOn: float, chance any cell is lit at start of game
- *
- * State:
- *
- * - hasWon: boolean, true when board is all off
- * - board: array-of-arrays of true/false
- *
- *    For this board:
- *       .  .  .
- *       O  O  .     (where . is off, and O is on)
- *       .  .  .
- *
- *    This would be: [[f, f, f], [t, t, f], [f, f, f]]
- *
- *  This should render an HTML table of individual <Cell /> components.
- *
- *  This doesn't handle any clicks --- clicks are on individual cells
- *
- **/
-
 class Board extends Component {
   static defaultProps = {
     nrows: 5,
@@ -94,7 +67,14 @@ class Board extends Component {
 
   render() {
     if (this.state.hasWon){
-      return <h1>YOU WON!!</h1>
+      return (
+        <div className="board-title">
+          <div className="winner">
+            <span className="neon-orange">YOU</span>
+            <span className="neon-blue">WON!</span>
+          </div>
+        </div>
+      )
     }
     // if the game is won, just show a winning msg & render nothing else
     let tblBoard = [];
@@ -107,9 +87,15 @@ class Board extends Component {
       tblBoard.push(<tr key={y}>{row}</tr>)
     }
     return (
-      <table className="Board">
-        <tbody>{tblBoard}</tbody>
-      </table>
+      <div>
+        <div className="board-title">
+          <div className="neon-orange">Lights</div>
+          <div className="neon-blue">Out</div>
+        </div>
+        <table className="Board">
+          <tbody>{tblBoard}</tbody>
+        </table>
+      </div>
     )
   }
 }
